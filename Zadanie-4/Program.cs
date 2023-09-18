17b257b04517a8dc4d9287e5014718022760136d
Rafal <rafal.ney@interia.pl>
2023-09-18 19:06:14 +0100

    Zadanie ukonczone

---
+++ Zadanie-4/Program.cs
@@ -0,0 +1,30 @@
+﻿string imie = "Ewa";
+char plec = 'K';
+int wiek = 33;
+
+if (wiek >= 30)
+{
+    Console.WriteLine($"{imie}, lat {wiek}");
+}
+else if (plec == 'K')
+{
+    if (wiek < 18)
+    {
+        Console.WriteLine("Niepelnoletnia Kobieta");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta ponizej 30 lat");
+    }
+}
+else if (plec != 'K')
+{
+    if (wiek < 18)
+    {
+        Console.WriteLine("Niepelnoletni Mezczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Mezczyzna ponizej 30 lat");
+    }
+}
